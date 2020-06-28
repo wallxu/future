9d595199e0db7ddde0300795ddc368fbbeff0f54
wallxu <xukf1@ziroom.com>
2020-06-28 10:13:54 +0800

    leetcode-第一题

---
+++ wallxu-dataStructures/src/main/java/com/wallxu/leetcode/array/TwoSum.java
@@ -0,0 +1,36 @@
+package com.wallxu.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. 两数之和
+ *
+ * @author: xukf
+ * @email: xukf1@ziroom.com
+ * @date: 2020/6/28 10:10
+ * @since 1.0.0
+ */
+public class TwoSum {
+
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> map = new HashMap<>();
+            int[] result = new int[2];
+            for (int i = 0; i < nums.length; i++){
+                map.put(nums[i], i);
+            }
+
+            for(int i=0; i<nums.length; i++){
+                //map里是否有另一个数
+                if(map.containsKey(target-nums[i]) && (map.get(target-nums[i]) != i)){
+                    result[1] = i;
+                    result[0] = map.get(target-nums[i]);
+                }
+            }
+
+            // System.out.println(result[0] + "," + result[1]);
+            return result;
+        }
+    }
+}
